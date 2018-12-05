9383c6b5af547ef1d6aec6fef33af5066e340f25
attila5287 <42498753+attila5287@users.noreply.github.com>
2018-12-05 14:05:33 -0700

    code_mod

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
+// DU CODING BOOTCAMP HW14 ATTILA
 
-
-// YOUR CODE HERE!
+//DATA DRIVEN DOCUMENTS-D3.SELECT
 
 var filterButton = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
@@ -9,6 +9,7 @@ var inputState = d3.select("#state");
 var inputCountry = d3.select("#country");
 var inputShape = d3.select("#shape");
 
+// UPDATES TABLE PER USE INPUT
 filterButton.on("click", updateTable);
 inputDate.on("change", updateTable);
 inputCity.on("change", updateTable);
@@ -17,41 +18,41 @@ inputCountry.on("change", updateTable);
 inputShape.on("change", updateTable);
 
 
-
+// DEFINE MAIN FUNCTION
 
 function updateTable() {
 
-	// Prevent the page from refreshing
-	d3.event.preventDefault();
+	// d3.event.preventDefault();
 
 	var mesa = d3.select(".mesa");
 
 
-	// from data.js
+	// import data from json formatted data file
 	var tableData = data;
 
-	//filter elements  ///also checks for strings with only spaces ||.replace(/\s/g, '')||
+	//filters date per user input entered on form on index page
 	if (inputDate.property("value").replace(/\s/g, ''))
 		tableData = tableData.filter(element => (element.datetime === inputDate.property("value")));
 
+	//filters city per user input entered on form on index page
 	if (inputCity.property("value").replace(/\s/g, ''))
 		tableData = tableData.filter(element => (element.city === inputCity.property("value")));
-
+	//filters state per user input entered on form on index page
 	if (inputState.property("value").replace(/\s/g, ''))
 		tableData = tableData.filter(element => (element.state === inputState.property("value")));
-
+	//filters country per user input entered on form on index page
 	if (inputCountry.property("value").replace(/\s/g, ''))
 		tableData = tableData.filter(element => (element.country === inputCountry.property("value")));
-
+	//filters shape of reported object per user input entered on form on index page
 	if (inputShape.property("value").replace(/\s/g, ''))
 		tableData = tableData.filter(element => (element.shape === inputShape.property("value")));
 
 
-	//clears any data that was already there
+	//clears input
 	mesa.selectAll('tr').remove();
 	mesa.selectAll('td').remove();
 
-	//copy pasta of 14.3.03-Evr_D3_Table   mmmm pasta
+	//call back function for iteration
 	tableData.forEach((element) => {
 		var row = mesa.append("tr");
 
